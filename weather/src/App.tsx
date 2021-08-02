9a15e031b56df9702b9da3c84b96493b22f7063d
goldking <>
2021-08-02 10:07:43 +0800

    Feat: 迁移 vite

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import Index from './views';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-        <Index />
+      <Index />
     </div>
   );
 }
